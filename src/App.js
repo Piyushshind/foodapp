001988dc5e2e160602fd32f01489cf56ebba4ce0
Piyushshind <piyushshinde.199@gmail.com>
2023-05-01 01:11:58 +0530

    footer shifted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Footer from './Componants/Footer/Footer';
 import Home from './Pages/Home/Home';
 import  { Route , Routes} from 'react-router-dom'
 function App() {
@@ -8,7 +7,6 @@ function App() {
      <Routes>
 
       <Route path='/' element={<Home />}/>
-      <Route path='/footer' element={<Footer />}/>
      </Routes>
       
     </>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../Componants/Navbar/Navbar'
+import Footer from '../../Componants/Footer/Footer'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
