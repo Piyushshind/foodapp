7bb1a56801251740b64dbe89fe04657f20ceec2e
Piyushshind <piyushshinde.199@gmail.com>
2023-05-01 13:08:10 +0530

    added font style

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid ">
+    <Link className="navbar-brand fs-1 fst-itallic" to="#">Swiggy</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active fs-4" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <Link className="nav-link fs-4" to="#">Login</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
     </div>
   </div>
