488c81848fd184f2c210660666e0176bfdb6aaee
Piyushshind <piyushshinde.199@gmail.com>
2023-05-01 16:25:38 +0530

    carosel changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './Pages/Home/Home';
-import  { Route , Routes} from 'react-router-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+// import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
 function App() {
   return (
     <>
-     <Routes>
-
-      <Route path='/' element={<Home />}/>
-     </Routes>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Navbar from '../../Componants/Navbar/Navbar'
 import Footer from '../../Componants/Footer/Footer'
 import Card from '../../Componants/Card/Card'
+import Carousel from '../../Componants/Carousel/Carousel'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div>
+        <Carousel />
+      </div>
       <div>
         <Card />
       </div>
