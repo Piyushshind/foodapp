b8f3b78e5869ace571e38ce54d0b979d6aeacb1c
Piyushshind <piyushshinde.199@gmail.com>
2023-05-12 23:03:06 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+// const mongoose=require('mongoose')
 const app = express();
-const dataBase = require("./DataBase");
+const dataBase = require("./db");
+// const mongoose = require('mongoose');
 
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect("mongodb+srv://foodees:foodees9270@cluster0.zpraavm.mongodb.net/foodees?retryWrites=true&w=majority");
+//   console.log('mongo');
+
+// }
 
 dataBase()
 
-app.get('/foodapp', (req,res)=>{
-       res.send('Hellow world')
+app.get('/foodapp', (req, res) => {
+  res.send('Hellow world')
 });
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
   console.log('server started');
 })
