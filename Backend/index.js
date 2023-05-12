67a2df9154ddb13aa08f950aaec7ff710531d452
Piyushshind <piyushshinde.199@gmail.com>
2023-05-12 23:33:15 +0530

    port changed

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,6 @@ app.use(express.json())
 app.use("/api",require("./Routes/CreateUser"))
 app.use("/api",require("./Routes/DisplayData"))
 
-// app.listen(9000, () => {
-//   console.log('node started')
-// })
+app.listen(5000, () => {
+  console.log('node started')
+})
