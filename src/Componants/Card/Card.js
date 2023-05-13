7b84a3b50ee18b28fce24b30b8b09339040ba303
Piyushshind <piyushshinde.199@gmail.com>
2023-05-13 15:41:21 +0530

    carousel transfered to home

--- src/Componants/Card/Card.js
+++ src/Componants/Card/Card.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+   const {foodName,options,imgSrc} = props;
+   const priceOption = Object.keys(options)
+
   return (
     <div>
         <div className="card mt-3" style={{ width: '18rem', maxHeight: '22.5rem' }}>
-          <img className="card-img-top" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rfcfoudtda1n7qqgbi4c" alt="food" />
+          <img className="card-img-top" src={imgSrc} alt="food" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">{foodName}</h5>
             <p className="card-text">Some quick example text to build on </p>
             <div className='container w-100'>
               <select className='m-2 h-100 bg-success rounded'>
                 {Array.from(Array(3), (e, i) => <option key={i + 1} value={i + 1}>{i + 1}</option>)}
               </select>
               <select className='m-2 h-100 bg-success rounded'>
-                <option value='half'>half</option>
-                <option value='full'>full</option>
+                 {
+                   priceOption.map(data => <option key={data} value={data}>{data}</option>)
+                 }
               </select>
               <div className='d-inline h-100 fs-5'>
                 Total price
@@ -26,4 +30,18 @@ const Card = () => {
   )
 }
 
-export default Card
+export default Card;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rfcfoudtda1n7qqgbi4c" 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,37 +2,93 @@ import React, { useEffect } from 'react'
 import Navbar from '../../Componants/Navbar/Navbar'
 import Footer from '../../Componants/Footer/Footer'
 import Card from '../../Componants/Card/Card'
-import Carousel from '../../Componants/Carousel/Carousel'
 import { useState } from 'react'
 
 const Home = () => {
-   
-   const [foodCat,setFoodCat] = useState([]);
-   const [foodItem,setFoodItem] = useState([]);
 
-   const loadData = async() => {
-      const response = await fetch("http://localhost:5000/api/foodData", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-     let fetchData = await response.json();
-      console.log(fetchData[0],fetchData[1]);
-   }
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
 
-   useEffect(()=>{
-      loadData()
-   },[])
+  const loadData = async () => {
+    const response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    let fetchData = await response.json();
+    setFoodItem(fetchData[0]);
+    setFoodCat(fetchData[1]);
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
 
   return (
     <>
       <Navbar />
       <div>
-        <Carousel />
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: 'contain !important' }}>
+          <div className="carousel-inner" id='carousel'>
+            <div className='carousel-caption' style={{ zIndex: '5' }}>
+              <div className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success text-white bg-success " type="submit">Search</button>
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img src="https://source.unsplash.com/random/900×700/?cricket" className="d-block w-100" style={{ filter: 'brightness(80%)' }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/900×700/?pizza" className="d-block w-100" style={{ filter: 'brightness(60%)' }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/900×700/?chicken" className="d-block w-100" style={{ filter: 'brightness(85%)' }} alt="..." />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
-      <div className='m-3'>
-        <Card />
+
+      
+      <div className='container'>
+        {
+          foodCat !== [] ? foodCat.map((data) => {
+            return (
+
+              <div className='row mb-3'>
+                <div key={data._id} className='fs-3 m-3'>
+                  {data.CategoryName}
+                </div>
+                <hr />
+                {
+                  foodItem !== [] ? foodItem.filter((item) => item.CategoryName === data.CategoryName)
+                    .map((filterItem) => {
+                      return (
+                        <div key={filterItem._id} className='col-12 col-md-6 col-lg-3'>
+                          <Card
+                            foodName={filterItem.name}
+                            options={filterItem.options[0]}
+                            imgSrc={filterItem.img}
+                          ></Card>
+                        </div>
+                      )
+                    })
+                    : ""
+                }
+              </div>
+
+            )
+          }) : ""
+        }
       </div>
       <Footer />
     </>
